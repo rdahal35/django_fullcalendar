b69e27d8ca0b2aa62512a18457bc537807b0615a
Rupesh Dahal <rdahal35@gmail.com>
2018-07-31 10:17:02 +0545

    adding event

--- mycalendar/views.py
+++ mycalendar/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def CalendarView(request):
 	return render(request, 'calendar.html')
